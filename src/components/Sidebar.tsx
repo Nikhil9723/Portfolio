c9f0f8e1cd53c8f4aa084b5eb964c06d4be62535
Nikhil Prajapati <nikhil.prajapati@simformsolutions.com>
2025-06-04 19:16:16 +0530

    feature/layout: Add Sidebar.tsx component file

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import githubLogo from '../assets/Github.svg'
+import dribble from '../assets/Dribble.svg'
+
+export default function Sidebar() {
+  return (
+    <div className='fixed top-0 left-7 flex flex-col justify-center gap-0.5'>
+        <div className='ml-3.5 w-0.5 h-[150px] border-1 bg-[#ABB2BF] border-red-200'></div>
+       <button> <img src={githubLogo}/></button>
+       <button><img src={dribble}/></button>
+    </div>
+        
+  )
+}
